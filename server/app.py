d9e13f4ba606d4e61b0e98155a6f8973c2c58135
Raphael Njoroge <raphael.njoroge@student.moringaschool.com>
2025-12-17 16:31:28 +0300

    First commit

--- server/app.py
+++ server/app.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
-
+# server/app.py
 from flask import Flask
 
 app = Flask(__name__)
+
+# Route for the homepage
+@app.route('/')
+def index():
+    return '<h1>Welcome to my page!</h1>'
+
+# Route for dynamic user profiles
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1>Profile for {username}</h1>'
+
+# This block allows you to run the app using 'python app.py'
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
--- server/testing/codegrade_test.py
+++ server/testing/codegrade_test.py
@@ -1,3 +1,4 @@
+# server/testing/codegrade_testing.py
 def test_codegrade_placeholder():
     """Codegrade placeholder test"""
     assert 1==1
